327b4f31b0f24043d952378289d18178cd8ef677
Anastasiia Tiutiun <tiutiunanastasia@gmail.com>
2026-03-06 16:51:13 +0100

    initial setup of angular v21 and angular material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule, 
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatCardModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
