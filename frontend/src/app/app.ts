573d29a44e859bb8d1d2e7244a0d755e492a3b36
Anastasiia Tiutiun <tiutiunanastasia@gmail.com>
2026-03-12 20:38:02 +0100

    create v1 of header component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,18 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { Header } from './shared/header/header';
 
 @Component({
   selector: 'app-root',
   imports: [
     CommonModule, 
-    MatToolbarModule, 
-    MatButtonModule, 
-    MatIconModule, 
-    MatCardModule],
+    Header
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
