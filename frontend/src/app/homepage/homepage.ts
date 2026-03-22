69a707c74a5b22cdf52be246d37d6b654d2c8b95
Tundra122 <118690898+Tundra122@users.noreply.github.com>
2026-03-22 23:07:01 +0200

    Test 2 Homepage

--- frontend/src/app/homepage/homepage.ts
+++ frontend/src/app/homepage/homepage.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { Header } from '../shared/header/header';
 import { BannerSectionComponent } from './banner-section/banner-section';
-import { ProductGridComponent } from '../product-catalog/product-grid/product-grid';
 import { CategoryCard } from './category-card/category-card';
 import { Product } from '../shared/product-card/product-card';
+import { ProductSliderComponent } from '../shared/product-slider/product-slider';
 
 @Component({
   selector: 'app-homepage',
@@ -11,7 +11,7 @@ import { Product } from '../shared/product-card/product-card';
   imports: [
     Header,
     BannerSectionComponent,
-    ProductGridComponent,
+    ProductSliderComponent,
     CategoryCard
   ],
   templateUrl: './homepage.html',
@@ -26,10 +26,45 @@ export class Homepage {
   readonly products: Product[] = [
     {
       imageUrl: 'assets/products/noodles.png',
-      name: 'Вермішель швидкого приготування',
-      description: 'Зі смаком яловичини',
-      price: 42,
+      name: 'Вермішель «Мівіна»',
+      description: 'з соусом солодкий чилі',
+      price: 42.0,
       unit: '75 г'
+    },
+    {
+      imageUrl: 'assets/products/reeva.png',
+      name: 'Вермішель Reeva',
+      description: 'зі смаком курки',
+      price: 18.69,
+      unit: '60 г'
+    },
+    {
+      imageUrl: 'assets/products/pelmeni.png',
+      name: 'Пельмені «Три ведмеді»',
+      description: 'зі смаком індички',
+      price: 310.0,
+      unit: '800 г'
+    },
+    {
+      imageUrl: 'assets/products/varenyky.png',
+      name: 'Вареники «Три Ведмеді»',
+      description: 'з картоплею',
+      price: 70.0,
+      unit: '400 г'
+    },
+    {
+      imageUrl: 'assets/products/puree.png',
+      name: 'Пюре Reeva',
+      description: 'швидкого приготування',
+      price: 38.2,
+      unit: '60 г'
+    },
+    {
+      imageUrl: 'assets/products/cordero.png',
+      name: 'Полента Cordero',
+      description: 'швидкого приготування',
+      price: 90.0,
+      unit: '250 г'
     }
   ];
 
