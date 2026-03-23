ee26cd3d2962a4df13ba1c1de6d6677588bcc800
AyTee001 <108694717+AyTee001@users.noreply.github.com>
2026-03-23 23:34:51 +0100

    insert homepage in routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { AboutUs } from './about-us/about-us/about-us';
 import { ProductPanel } from './product/product/product-panel'; 
 import { ProductCatalog } from './product-catalog/product-catalog/product-catalog';
+import { Homepage } from './homepage/homepage';
 
 export const routes: Routes = [
-  { path: '', component: AboutUs},
+  { path: '', component: Homepage},
   { path: 'about', component: AboutUs },
 
   { path: 'category/:categoryId', component: ProductCatalog },
