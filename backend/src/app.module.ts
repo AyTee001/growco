6a6ceb3d77b1eb98cdaa8e9bd96191fb6e87d071
AyTee001 <108694717+AyTee001@users.noreply.github.com>
2026-03-28 21:34:00 +0100

    fix product controller and db conenctions

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,17 +5,6 @@ import { AppService } from './app.service';
 import { CategoriesModule } from './сategories/сategories.module';
 import { CartModule } from './cart/cart.module';
 import { DeliverySlotsModule } from './delivery-slots/delivery-slots.module';
-import { Addresses } from './entities/Addresses';
-import { Cart } from './entities/Cart';
-import { CartItems } from './entities/CartItems';
-import { Categories } from './entities/Categories';
-import { DeliverySlots } from './entities/DeliverySlots';
-import { LoyaltyTransactions } from './entities/LoyaltyTransactions';
-import { OrderItems } from './entities/OrderItems';
-import { Orders } from './entities/Orders';
-import { Products } from './entities/Products';
-import { Stores } from './entities/Stores';
-import { Users } from './entities/Users';
 import { ProductsModule } from "./products/products.module";
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
@@ -27,30 +16,10 @@ import { appDataSourceOptions } from './config/typeorm';
       rootPath: join(process.cwd(), 'uploads'),
       serveRoot: '/uploads',
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5438,
-      username: 'postgres',
-      password: '12345',
-      database: 'postgres',
-      synchronize: false,
-      entities: [
-        Addresses,
-        Cart,
-        CartItems,
-        Categories,
-        DeliverySlots,
-        LoyaltyTransactions,
-        OrderItems,
-        Orders,
-        Products,
-        Stores,
-        Users,
-      ],
-    }), TypeOrmModule.forRoot(appDataSourceOptions),
+    TypeOrmModule.forRoot(appDataSourceOptions),
     CategoriesModule,
     CartModule,
+    ProductsModule,
     DeliverySlotsModule,
   ],
   controllers: [AppController],
