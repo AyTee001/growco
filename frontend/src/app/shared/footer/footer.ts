8fea39dd169f1f9bfd5600025f42484fdda3b4b7
AyTee001 <108694717+AyTee001@users.noreply.github.com>
2026-03-29 11:22:44 +0200

    adjust some styling

--- frontend/src/app/shared/footer/footer.ts
+++ frontend/src/app/shared/footer/footer.ts
@@ -10,20 +10,4 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class Footer {
   currentYear = signal(new Date().getFullYear());
-
-  leftLinks = [
-    { icon: 'info', label: 'Про нас', link: '#' },
-    { icon: 'work', label: 'Кар’єра', link: '#' },
-    { icon: 'event', label: 'Новини', link: '#' },
-    { icon: 'article', label: 'Блог', link: '#' },
-    { icon: 'groups', label: 'Наша команда', link: '#' }
-  ];
-
-  rightLinks = [
-    { icon: 'chat', label: 'Підтримка', link: '#' },
-    { icon: 'mail', label: 'Проконтактуйте з нами', link: '#' },
-    { icon: 'shield', label: 'Політика конфіденційності', link: '#' },
-    { icon: 'gavel', label: 'Умови користування сервісом', link: '#' },
-    { icon: 'help', label: 'FAQ', link: '#' }
-  ];
 }
