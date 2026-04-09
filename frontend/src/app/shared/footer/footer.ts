744cda235675c6f739d11409505a2e4b3dcb8d0e
Alex Shin <aldmitrshin@gmail.com>
2026-04-09 21:46:11 +0100

    Header, categories of products, info-block, Footer

--- frontend/src/app/shared/footer/footer.ts
+++ frontend/src/app/shared/footer/footer.ts
@@ -1,13 +1,34 @@
 import { Component, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
+
+interface FooterSections {
+  contacts: boolean;
+  company: boolean;
+}
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [
+    MatIconModule,
+    NgIf
+  ],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrls: ['./footer.scss'],
 })
 export class Footer {
   currentYear = signal(new Date().getFullYear());
+
+  openSections = signal<FooterSections>({
+    contacts: false,
+    company: false,
+  });
+
+  toggleSection(section: keyof FooterSections) {
+    this.openSections.update(prev => ({
+      ...prev,
+      [section]: !prev[section],
+    }));
+  }
 }
